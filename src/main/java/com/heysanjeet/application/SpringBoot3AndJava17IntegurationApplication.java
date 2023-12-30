c7cbd5eed998c4fbfc77967db1f269b1d6233f16
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-30 20:10:40 +0530

    initial commit

---
+++ src/main/java/com/heysanjeet/application/SpringBoot3AndJava17IntegurationApplication.java
@@ -0,0 +1,13 @@
+package com.heysanjeet.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3AndJava17IntegurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3AndJava17IntegurationApplication.class, args);
+	}
+
+}
