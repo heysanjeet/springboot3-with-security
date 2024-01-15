14a66205d1c01272f5e8a59be6341b5f00a5db2f
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-16 01:07:37 +0530

    curd implementation

---
+++ src/main/java/com/heysanjeet/application/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.heysanjeet.application.service;
+
+import com.heysanjeet.application.domain.UserInfo;
+import com.heysanjeet.application.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserInfo saveUser(UserInfo userInfo) {
+        return userRepository.save(userInfo);
+    }
+
+    @Override
+    public UserInfo getUserById(Integer userId) {
+        Optional<UserInfo> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
+    }
+
+    @Override
+    public List<UserInfo> getAllUsers() {
+        List<UserInfo> allUser = userRepository.findAll();
+        return allUser;
+    }
+
+    @Override
+    public UserInfo updateUser(UserInfo userInfo) {
+        UserInfo existingUser = userRepository.findById(userInfo.getUserId()).get();
+        existingUser.setUsername(userInfo.getUsername());
+        existingUser.setUserEmailId(userInfo.getUserEmailId());
+        existingUser.setUserPassword(userInfo.getUserPassword());
+        UserInfo updatedUser = userRepository.save(existingUser);
+        return updatedUser;
+    }
+
+    @Override
+    public void deleteUser(Integer userId) {
+        userRepository.deleteById(userId);
+
+    }
+}
