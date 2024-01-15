8e878614f6142ee96e752880e0e91adb325d7df9
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-16 01:35:04 +0530

    curd implementation

--- src/main/java/com/heysanjeet/application/controller/UserController.java
+++ src/main/java/com/heysanjeet/application/controller/UserController.java
@@ -1,29 +1,52 @@
 package com.heysanjeet.application.controller;
 
 import com.heysanjeet.application.domain.UserInfo;
+import com.heysanjeet.application.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/user/v1", produces = "applicatio/json")
+@Slf4j
 public class UserController {
 
-    @GetMapping("/welcome")
-    public ResponseEntity<String> sayHello() {
-        String message = "Hello, Welcome...";
-        return new ResponseEntity<>(message, HttpStatus.OK);
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/saveUser")
-    public ResponseEntity<UserInfo> saveUser(@RequestBody UserInfo userInfo) {
+    public ResponseEntity<UserInfo> saveUserInfo(@RequestBody UserInfo userInfo) {
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        UserInfo user = userService.saveUser(userInfo);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    @PostMapping("/updateUser")
-    public ResponseEntity<UserInfo> updateUser(@RequestBody UserInfo userInfo) {
+    @GetMapping("/getUserById/{userId}")
+    public ResponseEntity<UserInfo> getUserById(@PathVariable("userId") Integer userId) {
+        UserInfo user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<UserInfo>> getAllUser(@RequestBody UserInfo userInfo) {
+        List<UserInfo> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @PutMapping("updateUserById/{userid}")
+    public ResponseEntity<UserInfo> updateuser(@PathVariable("userId") Integer userId, @RequestBody UserInfo userInfo) {
+        userInfo.setUserId(userId);
+        UserInfo user = userService.updateUser(userInfo);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("deleteUser/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") Integer userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User has been deleted", HttpStatus.OK);
     }
 }
--- src/main/java/com/heysanjeet/application/service/UserServiceImpl.java
+++ src/main/java/com/heysanjeet/application/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.heysanjeet.application.domain.UserInfo;
 import com.heysanjeet.application.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
