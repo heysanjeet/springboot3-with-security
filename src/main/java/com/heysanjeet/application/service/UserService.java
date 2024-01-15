e2012efdbcd2d61986d3c8181a9710341c11b8c5
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-16 01:07:27 +0530

    curd implementation

--- src/main/java/com/heysanjeet/application/service/UserService.java
+++ src/main/java/com/heysanjeet/application/service/UserService.java
@@ -1,4 +1,19 @@
 package com.heysanjeet.application.service;
 
+import com.heysanjeet.application.domain.UserInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface UserService {
+    public UserInfo saveUser(UserInfo userInfo);
+
+    public UserInfo getUserById(Integer userId);
+
+    public List<UserInfo> getAllUsers();
+
+    public UserInfo updateUser(UserInfo userInfo);
+
+    public void deleteUser(Integer userId);
 }
