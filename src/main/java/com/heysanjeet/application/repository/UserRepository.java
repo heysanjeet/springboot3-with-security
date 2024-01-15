86d782cee374df6fd50a463575c8833411b0a528
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-16 01:06:59 +0530

    implemented user repository and user service

--- src/main/java/com/heysanjeet/application/repository/UserRepository.java
+++ src/main/java/com/heysanjeet/application/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.heysanjeet.application.repository;
 
-public interface UserRepository {
+import com.heysanjeet.application.domain.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserInfo, Integer> {
 }
